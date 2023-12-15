5c12afdfefab9c34c833dd036f5eb7fae814aef2
ariefhanmaulana <ariefhanmaulana@gmail.com>
2023-12-16 01:27:03 +0700

    commit pertama

---
+++ application/models/M_admin.php
@@ -0,0 +1,28 @@
+<?php 
+class M_admin extends CI_Model{
+
+    public function get_admin(){
+        return $this->db->get('admin');
+        // perintah diatas sama seperti select * form tbl_produk
+    }
+
+    public function tambah_admin($data) {
+        $this->db->insert('admin',$data);
+    }
+
+    public function hapus_admin($id) {
+        $this->db->where('id_admin',$id);
+        $this->db->delete('admin');
+    }  
+
+    public function update_admin($data,$id) {
+        $this->db->where('id_admin',$id);
+        $this->db->update('admin',$data);
+    }
+
+    public function get_admin_by_id($kodecus) {
+        return $this->db->get_where('admin', array('id_admin' => $kodecus));
+    }
+}
+
+?>
